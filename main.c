a9615d8cafd93782727e076bad5c5ee5939918ac
methy-dino <gutotlima@gmail.com>
2025-02-13 23:00:37 -0300

    basic structure with imports needed

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int main(int argC, char**args){
+	return 0;
+}
